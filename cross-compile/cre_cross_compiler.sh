398af24df326d5c1edb06d6c36f01ba43914eb4e
smalldegree <smalldegree@sina.com>
2015-12-29 16:15:47 +0800

    added gcc,glibc,kernel handle

--- cross-compile/cre_cross_compiler.sh
+++ cross-compile/cre_cross_compiler.sh
@@ -17,6 +17,8 @@
 #   history:
 #           1)Created by He Jianfei on 2015-12-23 10:30:00.
 #
+#           2)Set the env handler to the front of check dependency, on 
+#             2015-12-29 12:45:00.
 #---------------------------------------------------------------------------
 
 
@@ -24,12 +26,41 @@
 #01.prepare resources and check dependency.
 #02.init environment.
 #03.compile binutils.
-#04.generate kernel headers.
-#05.compile gcc first without C lib support.
+#04.compile gcc first without C lib support.
+#05.generate kernel headers.
 #06.compile C lib for ARM arch, such as glibc.
 #07.compile gcc second with C lib support.
 #08.check toolchains config.
 #09.test HelloWorld program.
+#10.report the config.
+
+
+host_os=$(cat /etc/issue | head -n 1)
+host_kernel=$(uname -r)
+host_cpu=$(cat /proc/cpuinfo | grep 'model name' | awk -F ': ' '{print $2}')
+
+rc_binutils=binutils-2.25.tar.gz
+
+rc_gcc=gcc-4.4.7.tar.gz
+rc_glibc=glibc-2.11.tar.gz
+rc_glibc_ports=glibc-ports-2.11.tar.gz
+
+rc_gmp=gmp-4.2.tar.gz
+rc_mpfr=mpfr-2.4.0.tar.gz
+
+rc_linux_kernel=linux-2.6.32.27.tar.gz    
+
+
+echo 'export    TOP_DIR=/root/cross_compile' > /tmp/env.sh
+echo 'export    PRJROOT=$TOP_DIR/embeded_toolchains' >> /tmp/env.sh
+echo 'export    TARGET=arm-linux' >> /tmp/env.sh
+
+echo 'export    PREFIX=$PRJROOT/tool_chain' >> /tmp/env.sh
+echo 'export    TARGET_PREFIX=$PREFIX/$TARGET' >> /tmp/env.sh
+
+echo 'export    PATH=$PATH:$PREFIX/bin' >> /tmp/env.sh
+
+source /tmp/env.sh
 
 
 #01.prepare resources and check dependency.
@@ -40,7 +71,7 @@ function precheck()
     echo "Checking packages dependencies:"
     for i in bash grep sed awk perl gcc glibc make coreutils diffutils gettext texinfo
     do
-        rpm -qa | grep $i 2>&1 > /dev/null
+        rpm -qa | grep $i > /dev/null 2>&1
         rc=$?
 
         echo "    Checking whether has installed package ${i}: $([ $rc -eq 0 ] && echo yes || echo no)"
@@ -55,49 +86,23 @@ function precheck()
 #02.init environment.
 function init_env()
 {
-    host_os=$(cat /etc/issue | head -n 1)
-    host_kernel=$(uname -r)
-    host_cpu=$(cat /proc/cpuinfo | grep 'model name' | awk -F ': ' '{print $2}')
-
-    rc_binutils=binutils-2.25.tar.gz
-
-    rc_gcc=gcc-4.4.7.tar.gz
-    rc_glibc=glibc-2.11.tar.gz
-    rc_glibc_ports=glibc-ports-2.11.tar.gz
-
-    rc_gmp=gmp-4.2.tar.gz
-    rc_mpfr=mpfr-2.4.0.tar.gz
-
-    rc_linux_kernel=linux-2.6.32.tar.gz    
-
-
-    export    TOP_DIR=/root/cross_compile
-    export    PRJROOT=$TOP_DIR/embeded_toolchains
-    export    TARGET=arm-linux
-
-    export    PREFIX=$PRJROOT/tool_chain
-    export    TARGET_PREFFIX=$PREFIX/$TARGET
-
-    export    PATH=$PATH:$PREFIX/bin
-
-
-    rm -rf $TOP_DIR && mkdir $TOP_DIR
-    mkdir $PRJROOT
+    mkdir $TOP_DIR > /dev/null 2>&1
+    mkdir $PRJROOT > /dev/null 2>&1
    
-    mkdir $PRJROOT/setup_dir
-    mkdir $PRJROOT/src_dir
+    mkdir $PRJROOT/setup_dir > /dev/null 2>&1
+    mkdir $PRJROOT/src_dir > /dev/null 2>&1
     
-    mkdir $PRJROOT/build_dir
-    mkdir $PRJROOT/build_dir/build_binutils
-    mkdir $PRJROOT/build_dir/build_gcc
-    mkdir $PRJROOT/build_dir/build_glibc
+    mkdir $PRJROOT/build_dir > /dev/null 2>&1
+    mkdir $PRJROOT/build_dir/build_binutils > /dev/null 2>&1
+    mkdir $PRJROOT/build_dir/build_gcc > /dev/null 2>&1
+    mkdir $PRJROOT/build_dir/build_glibc > /dev/null 2>&1
     
-    mkdir $PRJROOT/kernel
+    mkdir $PRJROOT/kernel > /dev/null 2>&1
     
-    mkdir $PRJROOT/tool_chain
+    mkdir $PRJROOT/tool_chain > /dev/null 2>&1
 
-    mkdir $PRJROOT/doc
-    mkdir $PRJROOT/program
+    mkdir $PRJROOT/doc > /dev/null 2>&1
+    mkdir $PRJROOT/program > /dev/null 2>&1
     
     
     echo "Checking the files' existence:" 
@@ -105,13 +110,13 @@ function init_env()
     do
         if [ -f ./$i ]
         then
-            cp ./$i $PRJROOT/setup_dir/ && echo "    File ${i}: exists"
+            cp -an ./$i $PRJROOT/setup_dir/ && echo "    File ${i}: exists"
         elif [ -f /tmp/$i ]
         then
-            cp ./$i $PRJROOT/setup_dir/ && echo "    File ${i}: exists"
+            cp -an ./$i $PRJROOT/setup_dir/ && echo "    File ${i}: exists"
         elif [ -f ~/$i ]
         then
-            cp ./$i $PRJROOT/setup_dir/ && echo "    File ${i}: exists"
+            cp -an ./$i $PRJROOT/setup_dir/ && echo "    File ${i}: exists"
         else
             echo "    File ${i}: Not found in current dir or /tmp/ or ~/" && exit
         fi
@@ -119,13 +124,13 @@ function init_env()
 
     if [ -f ./$rc_linux_kernel ]
     then
-        cp ./$rc_linux_kernel $PRJROOT/kernel/ && echo "    File ${rc_linux_kernel}: exists"
+        cp -an ./$rc_linux_kernel $PRJROOT/kernel/ && echo "    File ${rc_linux_kernel}: exists"
     elif [ -f /tmp/$rc_linux_kernel ]
     then
-        cp ./$rc_linux_kernel $PRJROOT/kernel/ && echo "    File ${rc_linux_kernel}: exists"
+        cp -an ./$rc_linux_kernel $PRJROOT/kernel/ && echo "    File ${rc_linux_kernel}: exists"
     elif [ -f ~/$rc_linux_kernel ]
     then
-        cp ./$rc_linux_kernel $PRJROOT/kernel/ && echo "    File ${rc_linux_kernel}: exists"
+        cp -an ./$rc_linux_kernel $PRJROOT/kernel/ && echo "    File ${rc_linux_kernel}: exists"
     else
         echo "    File ${rc_linux_kernel}: Not found in current dir or /tmp/ or ~/" && exit
     fi
@@ -140,6 +145,8 @@ function init_env()
     echo "    Unziping the file $rc_linux_kernel ..."
     tar -zxvf $PRJROOT/kernel/$rc_linux_kernel -C $PRJROOT/kernel/ > /dev/null
     
+    cp -an /tmp/env.sh   $PRJROOT/doc/
+
     echo -e "\033[32mInit environment successfully.\033[0m"
 }
 
@@ -155,47 +162,121 @@ function compile_binutils()
 
     make && make install
 
-    [ $? -eq 0 ] && echo -e "\033[32mCompiled binutils successfully.\033[0m"
+    [ $? -gt 0 ] && echo -e "\033[32mCompiled binutils successfully.\033[0m"
 }
 
 
-#04.generate kernel headers.
+#04.compile gcc first without C lib support.
+function compile_gcc_without_c_lib()
+{
+    echo ""
+    echo "Compiling gcc first: "
+
+    cp -an $PRJROOT/src_dir/${rc_gmp%%.tar.gz}       $PRJROOT/src_dir/${rc_gcc%%.tar.gz}/gmp
+    cp -an $PRJROOT/src_dir/${rc_mpfr%%.tar.gz}      $PRJROOT/src_dir/${rc_gcc%%.tar.gz}/mpfr
+
+    cd $PRJROOT/build_dir/build_gcc
+    sed -i 's/TARGET_LIBGCC2_CFLAGS.*/TARGET_LIBGCC2_CFLAGS = -fomit-frame-pointer -fPIC -Dinhibit_libc -D_gthr_posix.h/g' ../../src_dir/${rc_gcc%%.tar.gz}/gcc/config/t-linux
+
+    sh ../../src_dir/${rc_gcc%%.tar.gz}/configure --prefix=$PREFIX \
+                                                  --target=$TARGET \
+                                                  --without-headers \
+                                                  --enable-languages=c \
+                                                  --disable-shared \
+                                                  --disable-threads \
+                                                  --disable-decimal-float \
+                                                  --disable-libmudflap \
+                                                  --disable-libssp
+
+    make all-gcc
+    make install-gcc
+
+    make all-target-libgcc
+    make install-target-libgcc
+
+    str_temp=${rc_gcc%%.tar.gz}
+    cd $PREFIX/lib/gcc/arm-linux/${str_temp#gcc-}
+    ln -s libgcc.a libgcc_eh.a
+ 
+    [ $? -gt 0 ] && echo -e "\033[32mCompiled gcc without c lib support successfully.\033[0m"
+}
+
+
+#05.generate kernel headers.
 function generate_kernel_headers()
 {
     echo ""
     echo "Generating kernel headers: "
 
+    cd $PRJROOT/kernel/${rc_linux_kernel%%.tar.gz}
+    #make ARCH=arm CROSS_COMPILE=arm-linux- menuconfig    
+    #you can use a common file ".config", so you needn't to config kernel.
+    make ARCH=arm CROSS_COMPILE=arm-linux- 
+
+    mkdir -p $TARGET_PREFIX/include > /dev/null 2>&1
+    cp -an $PRJROOT/kernel/${rc_linux_kernel%%.tar.gz}/include/linux              $TARGET_PREFIX/include/
+    cp -an $PRJROOT/kernel/${rc_linux_kernel%%.tar.gz}/include/asm-arm            $TARGET_PREFIX/include/
+    cp -an $PRJROOT/kernel/${rc_linux_kernel%%.tar.gz}/include/asm-generic        $TARGET_PREFIX/include/
     
+    cp -an $PRJROOT/kernel/${rc_linux_kernel%%.tar.gz}/arch/arm/include/asm       $TARGET_PREFIX/include/
 
-    [ $? -eq 0 ] && echo -e "\033[32mGenerated kernel headers successfully.\033[0m"
-
+    [ $? -gt 0 ] && echo -e "\033[32mGenerated kernel headers successfully.\033[0m"
 }
 
 
-##05.compile gcc first without C lib support.
-#function compile_gcc_without_c_lib()
-#{
-#
-#
-#}
-#
-#
-#
-##06.compile C lib for ARM arch, such as glibc.
-#function compile_c_lib()
-#{
-#
-#}
-#
-#
-##07.compile gcc second with C lib support.
-#function compile_gcc_with_c_lib()
-#{
-#
-#}
-#
-#
-#
+#06.compile C lib for ARM arch, such as glibc.
+function compile_c_lib()
+{
+    echo ""
+    echo "Compiling glibc: "
+
+    cp -an $PRJROOT/src_dir/${rc_glibc_ports%%.tar.gz}      $PRJROOT/src_dir/${rc_glibc%%.tar.gz}/ports
+
+    cd $PRJROOT/build_dir/build_glibc
+    CC=arm-linux-gcc AR=arm-linux-ar RANLIB=arm-linux-ranlib
+    sh ../../src_dir/${rc_glibc%%.tar.gz}/configure --prefix=$PREFIX \
+                                                    --host=arm-linux \
+                                                    --enable-add-ons \
+                                                    --with-headers=$PREFIX/$TARGET/include \
+                                                    --with-tls \
+                                                    --disable-profile \
+                                                    libc_cv_forced_unwind=yes \
+                                                    libc_cv_c_cleanup=yes \
+                                                    libc_cv_arm_tls=yes
+
+    make
+    make install
+
+    [ $? -gt 0 ] && echo -e "\033[32mCompiled glibc successfully.\033[0m"
+
+
+#   注：以上完成后，请查看一下$TARGET_PREFIX/lib目录下的文件libc.so，看看GROUP的内容是否指定到可以用于交叉编译的库，如果不是请修改，如下。
+#   libc.so 文件(所在目录是$TARGET_PREFIX/lib)，将GROUP ( /lib/libc.so.6 /lib/libc_nonshared.a)改为GROUP ( libc.so.6 libc_nonshared.a)
+#这样连接程序 ld 就会在 libc.so 所在的目录查找它需要的库，因为你的机子的/lib目录可能已经装了一个相同名字的库，一个为编译可以在你的宿主机上运行的程序的库，而不是用于交叉编译的。
+}
+
+
+#07.compile gcc second with C lib support.
+function compile_gcc_with_c_lib()
+{
+    echo ""
+    echo "Compiling gcc second: "
+
+    cd $PRJROOT/build_dir/build_gcc
+    sed -i 's/TARGET_LIBGCC2_CFLAGS.*/TARGET_LIBGCC2_CFLAGS = -fomit-frame-pointer -fPIC/g' ../../src_dir/${rc_gcc%%.tar.gz}/gcc/config/t-linux
+    
+    sh ../../src_dir/${rc_gcc%%.tar.gz}/configure --prefix=$PREFIX \
+                                                  --target=$TARGET \
+                                                  --enable-shared \
+                                                  --enable-languages=c
+
+    make all
+    make install
+ 
+    [ $? -gt 0 ] && echo -e "\033[32mCompiled gcc with c lib support successfully.\033[0m"
+}
+
+
 ##08.check toolchains config.
 #function postcheck()
 #{
@@ -211,26 +292,61 @@ function generate_kernel_headers()
 #}
 
 
-precheck
-
-init_env
-
-#compile_binutils
-
-generate_kernel_headers
-
-
-function usage()
+#10.report the config.
+function report_config()
 {
+echo ""
 cat <<END 2>&1
-usage: ${0##*/} [on | off | status]
-       on      --   enable ipv6
-       off     --   disable ipv6
-       status  --   show ipv6 support status
+Configs as follow:
+
+    01.the script is used to build ARM cross_compile env based on CentOS 6.4 x86_64.
+
+    02.the main used resources as follow: default, you can use others
+        *binutils: ${rc_binutils} 
+        
+        *gcc: ${rc_gcc}
+        *glibc: ${rc_glibc}
+        *glibc_ports: ${rc_glibc_ports}
+
+        *gmp: ${rc_gmp}
+        *mpfr: ${rc_mpfr}
+
+        *linux_kernel: ${rc_linux_kernel}
+
+    03.the env as follow:
+        TOP_DIR: ${TOP_DIR}
+        PRJROOT: ${PRJROOT}
+        TARGET : ${TARGET}
+        
+        PREFIX : ${PREFIX}
+        TARGET_PREFIX: ${TARGET_PREFIX}
+
+        PATH   : ${PATH}
+    
+    04.the host info:
+        host_os:     ${host_os}
+        host_kernel: ${host_kernel}
+        host_cpu:    ${host_cpu}
+
 END
 }
 
 
-[ $# -eq 0 ] && usage && exit
+#precheck && sleep 2
+
+#init_env && sleep 2
+
+#compile_binutils && sleep 2
+
+#compile_gcc_without_c_lib && sleep 2
+
+#generate_kernel_headers && sleep 2
+
+#compile_c_lib && sleep 2
+
+compile_gcc_with_c_lib
+
+
+report_config
 
 
